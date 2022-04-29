e85db861aeb6fd29b3524d8565c01f840b9e42c7
Andre Bajorat <andre.bajorat@wizeline.com>
2022-04-29 00:51:10 -0500

    Reset

--- tec_api/authentication.py
+++ tec_api/authentication.py
@@ -1,12 +1,1 @@
 from fastapi import HTTPException, Request
-
-
-def verify_token(req: Request):
-    token = req.headers.get("Authorization", None)
-    valid = token == 'BestPossiblePassword'
-    if not valid:
-        raise HTTPException(
-            status_code=401,
-            detail="Unauthorized"
-        )
-    return True
