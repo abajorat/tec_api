16e5e5b8b315f207ca81dad409554feaa884e0fd
Andre Bajorat <andre.bajorat@wizeline.com>
2022-04-29 21:08:23 -0500

    Second

--- tec_api/main.py
+++ tec_api/main.py
@@ -34,6 +34,12 @@ song_db = ["evermore", "willow", "champagne problems", "gold rush",
            "happiness", "dorothea", "coney island",
            "ivy", "cowboy like me", "long story short"]
 
+@app.get("/songs/evermore")
+async def get_evermore():
+    return {"song_id": "0",
+            "song_name": "evermore"}
+    
+
 
 
 @app.get("/songs/{song_id}")
@@ -42,11 +48,7 @@ async def get_song(song_id: int = Path(
     return {"song_id": song_id, "song_name": song_db[song_id]}
 
 
-@app.get("/songs/evermore")
-async def get_evermore():
-    return {"song_id": "0",
-            "song_name": "evermore"}
-    
+
 
 @app.get("/album/{album_name}")
 async def get_album(album_name: AlbumName):
@@ -55,3 +57,8 @@ async def get_album(album_name: AlbumName):
     if album_name.value == "evermore":
         return {"album_name": album_name, "year": 2020}
     return {"album_name": album_name, "year": 2021}
+
+
+@app.get("/song_list")
+async def get_song_list(skip: int = 0, limit: int = 10):
+    return song_db[skip: skip + limit]
