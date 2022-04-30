52f5734cd644e168f3d3ad313e0064918b3f5b33
Andre Bajorat <andre.bajorat@wizeline.com>
2022-04-29 20:41:23 -0500

    First

--- tec_api/main.py
+++ tec_api/main.py
@@ -17,3 +17,12 @@ async def random_name():
 @app.get("/name/{name}")
 async def get_name(name):
     return {"message": f"Hello {name}"}
+
+song_db = ["evermore", "willow", "champagne problems", "gold rush",
+           "'tis the damn season'", "tolerate it", "no body no crime",
+           "happiness", "dorothea", "coney island",
+           "ivy", "cowboy like me", "long story short"]
+
+@app.get("/songs/{song_id}")
+async def get_song(song_id):
+    return {"song_id": song_id, "song_name": song_db[song_id]}
