cb03a7d568ce6abba18367c4b9001fa164ee9226
Andre Bajorat <abajorat@posteo.de>
2022-10-15 13:32:56 -0500

    albums

--- src/tec_api/main.py
+++ src/tec_api/main.py
@@ -2,8 +2,22 @@ from fastapi import FastAPI
 import names
 from pydantic import StrictStr
 from enum import Enum
+from pydantic import BaseModel, NegativeInt, PositiveInt, confloat, conlist, constr
 
 app = FastAPI()
+
+
+class Album(BaseModel):
+    name: str
+    description: str = "Default"
+    price: confloat(gt=0) = "No price given"
+    discount: float = None
+
+
+class AlbumOut(BaseModel):
+    name: str
+    description: str = None
+    new_price: float = None
 
 
 class AlbumName(str, Enum):
@@ -47,11 +61,12 @@ song_db = [
     "cowboy like me",
     "long story short",
 ]
+album_dict = {}
 
 
 @app.get("/songs")
-async def get_song():
-    return {"song_db": song_db}
+async def get_song(skip: int = 0, limit: int = 10):
+    return song_db[skip : skip + limit]
 
 
 @app.get("/songs/{song_id}")
@@ -66,3 +81,11 @@ async def get_album(album_name: AlbumName):
     if album_name.value == "evermore":
         return {"album_name": album_name, "year": 2020}
     return {"album_name": album_name, "year": 2021}
+
+
+@app.post("/albums", response_model=AlbumOut)
+async def create_album(album: Album):
+    album_dict = album.dict()
+    new_price = album.price - album.discount * album.price
+    album_dict.update({"new_price": new_price})
+    return album_dict
