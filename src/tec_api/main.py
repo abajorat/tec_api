6a70058b9f94984f56b6ce3ce1d0826d0a8212e0
Andre Bajorat <abajorat@posteo.de>
2022-10-15 12:58:03 -0500

    strictstr

--- src/tec_api/main.py
+++ src/tec_api/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import names
+from pydantic import StrictStr
 
 app = FastAPI()
 
@@ -12,6 +13,11 @@ async def root():
 @app.get("/random_name")
 async def random_name():
     return {"message": f"Hello {names.get_first_name()}"}
+
+
+@app.get("/name/{name}")
+async def get_name(name: StrictStr):
+    return {"message": f"Hello {name}"}
 
 
 song_db = [
@@ -29,6 +35,11 @@ song_db = [
     "cowboy like me",
     "long story short",
 ]
+
+
+@app.get("/songs")
+async def get_song():
+    return {"song_db": song_db}
 
 
 @app.get("/songs/{song_id}")
